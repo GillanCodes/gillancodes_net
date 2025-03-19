4716bafba08d2bfd4e658220a003e126b9adfaee
gillancodes <gillancodes@laposte.net>
2025-03-19 14:12:44 +0100

    feat: added proxy on vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        "target" : "http://localhost:5000",
+        "changeOrigin": false,
+        "secure": false
+      },
+      "/cdn": {
+        "target" : "http://localhost:5000",
+        "changeOrigin": false,
+        "secure": false
+      },
+      "/assets": {
+        "target" : "http://localhost:5000",
+        "changeOrigin": false,
+        "secure": false
+      },
+    }
+  }
 })
