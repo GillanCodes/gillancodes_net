205025edd553e20b9cd167f323ad19ada30e3d72
gillancodes <gillancodes@laposte.net>
2025-03-19 14:52:17 +0100

    refactor: move landing into landing

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,17 +1,21 @@
+import { useNavigate } from 'react-router';
 import './Landing.scss';
 
 export default function Landing() {
+  
+  const navigate = useNavigate();
 
   const close = (link: string) => {
     document.getElementById('landing')!.className = "landing is-close" 
+    if (link) navigate(link); 
   }
 
   return (
     <div className="landing" id="landing">
       <div className="landing-content">
         <div className="bento-grid">
-          <div className="box" id="one" onClick={() => close('/home')}></div>
-          <div className="box" id="two"></div>
+          <div className="box" id="one" onClick={() => close('/about')}></div>
+          <div className="box" id="two" onClick={() => close('/')}></div>
           <div className="box" id="three"></div>
           <div className="box" id="four"></div>
           <div className="box" id="five"></div>
