bcdeea1a996e0989a69a659d30c5685e0730e86f
gillancodes <gillancodes@laposte.net>
2025-03-25 13:22:42 +0100

    feat: added keybind to nav
    added escape to open or close the navbar

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,23 +1,45 @@
 import { useNavigate } from 'react-router';
 import './Landing.scss';
+import { useEffect } from 'react';
 
 export default function Landing() {
   
   const navigate = useNavigate();
 
-  const close = (link: string) => {
-    document.getElementById('landing')!.className = "landing is-close" 
+  const switchState = (link?: string) => {
+    var state = document.getElementById('landing');
+    if (!state) return;
+
     if (link) navigate(link); 
+
+    if (!state.className.includes('is-close')) return state.className = "landing is-close";
+    else return state.className = "landing is-open";
   }
+
+  useEffect(() => {
+    const keyDownHandler = (event:KeyboardEvent) => {
+      switch (event.key)
+      {
+        case "Escape":
+          return switchState();
+        default:
+          return
+      }
+    }
+    document.addEventListener("keydown", keyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    }
+  }, [])
 
   return (
     <div className="landing" id="landing">
       <div className="landing-content">
         <div className="bento-grid">
-          <div className="box" id="one" onClick={() => close('/about')}>
+          <div className="box" id="one" onClick={() => switchState('/about')}>
             <p>About</p>
           </div>
-          <div className="box" id="two" onClick={() => close('/')}>
+          <div className="box" id="two" onClick={() => switchState('/')}>
             <p>Home</p>
           </div>
           <div className="box" id="three">
