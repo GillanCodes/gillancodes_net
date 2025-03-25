429745edb7bbab5402100a0fba687d66ab0390d9
gillancodes <gillancodes@laposte.net>
2025-03-25 13:01:36 +0100

    feat: added style to home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,7 +6,13 @@ export default function Home() {
     <main className='container' id='home'>
       <NavOpen />
 
-      <section className='section'>
+      <section className='section home'>
+        <div className="home__title">
+          <h1><span className="highlight">GillanCodes</span><br/> Full-Stack Web Developer.</h1>
+        </div>
+        <div className='home__image'>
+          <img src="/assets/image_98.jpg" alt="" />
+        </div>
       </section>
 
       <section className='section'>
