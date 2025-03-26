423c7361b57f2bf7bca16e8ef651fc5970f90152
gillancodes <gillancodes@laposte.net>
2025-03-26 14:36:17 +0100

    feat: added app wapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import Routes from "./router/index";
 import "./styles/index.scss";
 
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes />
+      <div id='app'>
+        <Routes />
+      </div>
     </>
   )
 }
