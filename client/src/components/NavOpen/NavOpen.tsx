6a353a7c9a4c6ef2863c396b4570643803ce1de9
gillancodes <gillancodes@laposte.net>
2025-03-26 14:36:52 +0100

    fix: nav open btn

--- client/src/components/NavOpen/NavOpen.tsx
+++ client/src/components/NavOpen/NavOpen.tsx
@@ -1,12 +1,18 @@
+function switchState() {
+  var state = document.getElementById('landing');
+  if (!state) return;
+
+  if (!state.className.includes('is-close')) 
+    state.className = "landing is-close";
+  else 
+    state.className = "landing is-open";
+
+  return;
+} // TODO : Better system
 
 export default function NavOpen() {
-
-  const open = () => {
-    document.getElementById("landing")!.className = "landing is-open";
-  }
-
   return (
-    <button onClick={open}>Open Nav</button>  
+    <button onClick={() => switchState()}>Open Nav</button>  
   )
 }
 
